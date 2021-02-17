da89134c7ca778b0629905dec26c845e544549f3
arfpchatelot <fchatelot@yahoo.fr>
2021-02-17 11:09:36 +0100

    ajoute fonction heure exercice JS1

--- js/dategroupeb.js
+++ js/dategroupeb.js
@@ -20,6 +20,32 @@ chainedate+=jourmois;
 return chainedate;
 
 }
-let machaine= affichedate();
-console.log(machaine);
+
+
+/*let machaine= affichedate();*/
+/*console.log(machaine);*/
+
 //alert(affichedate());
+var objetboutondate= document.querySelector('#btndate');
+objetboutondate.addEventListener('click',function(){ document.getElementById('madate').value=affichedate();} );
+
+function afficheheure()
+{
+    var chaineheure='';
+    var monheure=new Date();
+    let heure=monheure.getHours();
+    (heure<10)? chaineheure+='0'+heure:chaineheure+=heure;
+    let min=monheure.getMinutes();
+    (min<10)?chaineheure+=':0'+min:chaineheure+=':'+min;
+    let sec=monheure.getSeconds();
+    (sec<10)?chaineheure+=':0'+sec:chaineheure+=':'+sec;
+
+    document.getElementById('monheure').value=chaineheure;
+    setTimeout(function() {afficheheure();
+        
+    }, 1000);
+    
+
+}
+var objetboutonheure=document.querySelector('#btnheure');
+objetboutonheure.addEventListener('click',function(){ afficheheure();});
