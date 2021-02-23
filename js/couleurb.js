4bcca8cca457a785b3af300e0606f3ba80d6a4d9
arfpchatelot <fchatelot@yahoo.fr>
2021-02-23 10:09:22 +0100

    Amelioration exercice couleur JS avec focus sur zone erreur

--- js/couleurb.js
+++ js/couleurb.js
@@ -1,75 +1,78 @@
-function couleurchange()
-{ 
-let chainecouleur= "#"+document.querySelector("#txtrouge").value+ document.querySelector("#txtvert").value+document.querySelector("#txtbleu").value;
-return chainecouleur;
+function couleurchange() {
+    let chainecouleur = "#" + document.querySelector("#txtrouge").value + document.querySelector("#txtvert").value + document.querySelector("#txtbleu").value;
+    return chainecouleur;
 
 }
 
-function controleHexa( chainehexa)
-{
-    chainehexa=chainehexa.toUpperCase();
-  var maregex=/^[A-F0-9]{2}$/;
-  let boolres=maregex.test(chainehexa);
- 
- console.log(boolres);
- return boolres;
-}    
+function controleHexa(chainehexa) {
+    chainehexa = chainehexa.toUpperCase();
+    var maregex = /^[A-F0-9]{2}$/;
+    let boolres = maregex.test(chainehexa);
+    console.log(boolres);
+    return boolres;
+}
 
-var objtxtrouge=document.querySelector("#txtrouge");
+var objtxtrouge = document.querySelector("#txtrouge");
 
 objtxtrouge.addEventListener("blur", function () {
 
-       if(controleHexa(this.value)==false)
-       {
-           this.value="";
-           this.style.backgroundColor="#880000";
-          
+    if (controleHexa(this.value) == false) {
+        this.value = "";
+        this.style.backgroundColor = "#880000";
+        this.style.color = "#FFFFFF"
+        this.focus();
+
         alert("veuiller saisir un chiffre entre 0 et 9, ou une lettre de A à F");
-       }
-       else
-       {
-        this.style.backgroundColor="#ffffff";
-        document.body.style.backgroundColor=couleurchange();
-       }
-        });
-       
-       
+    }
+    else {
+        this.style.backgroundColor = "#ffffff";
+        this.style.color = '#000000';
+        document.body.style.backgroundColor = couleurchange();
+    }
+});
 
 
-   
 
-    //-----------------------
-    var objtxtvert=document.querySelector("#txtvert");
+
+
+
+//-----------------------
+var objtxtvert = document.querySelector("#txtvert");
 
 objtxtvert.addEventListener("blur", function () {
 
-    if(controleHexa(this.value)==false)
-    {
-        this.value="";
-        this.style.backgroundColor="#880000";
-       
-     alert("veuiller saisir un chiffre entre 0 et 9, ou une lettre de A à F");
-    }
-    else
-    {
-     this.style.backgroundColor="#ffffff";
-     document.body.style.backgroundColor=couleurchange();
-    };});
+    if (controleHexa(this.value) == false) {
+        this.value = "";
+        this.style.backgroundColor = "#880000";
+        this.style.color = "#FFFFFF";
+        this.focus();
 
-    ///------------------
-    var objtxtbleu=document.querySelector("#txtbleu");
+        alert("veuiller saisir un chiffre entre 0 et 9, ou une lettre de A à F");
+    }
+    else {
+        this.style.backgroundColor = "#ffffff";
+        this.style.color = '#000000';
+        document.body.style.backgroundColor = couleurchange();
+
+    };
+});
+
+///------------------
+var objtxtbleu = document.querySelector("#txtbleu");
 
 objtxtbleu.addEventListener("blur", function () {
 
-    if(controleHexa(this.value)==false)
-    {
-        this.value="";
-        this.style.backgroundColor="#880000";
-       
-     alert("veuiller saisir un chiffre entre 0 et 9, ou une lettre de A à F");
+    if (controleHexa(this.value) == false) {
+        this.value = "";
+        this.style.backgroundColor = "#880000";
+        this.style.color = "#FFFFFF";
+        this.focus();
+
+        alert("veuiller saisir un chiffre entre 0 et 9, ou une lettre de A à F");
     }
-    else
-    {
-     this.style.backgroundColor="#ffffff";
-     document.body.style.backgroundColor=couleurchange();
-    };});
+    else {
+        this.style.backgroundColor = "#ffffff";
+        this.style.color = '#000000';
+        document.body.style.backgroundColor = couleurchange();
+    };
+});
